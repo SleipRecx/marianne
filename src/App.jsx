4a5719a92795476cb163590f0007073f87296661
Markus Andresen <markus.andresen94@gmail.com>
2022-07-11 17:05:56 +0200

    wip2

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
        <P5Wrapper sketch={sketch} />
+       
       </header>
     </div>
   );
